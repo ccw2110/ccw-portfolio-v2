f16b3b9f100ab13042fb17f2aa5e85d4c93a2723
ccw2110 <carloscassinelli90@gmail.com>
2024-08-08 13:17:48 -0500

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Carlos Cassinelli PortFolio",
+  title: "Carlos Cassinelli PortFolio ",
   description: "",
 };
 
