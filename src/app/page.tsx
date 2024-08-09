691a0c679523edd183e7c1a17267a843eb09ea67
ccw2110 <carloscassinelli90@gmail.com>
2024-08-09 00:23:04 -0500

    hero and about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default function Home() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Hero />
+      <About/>
+      
+    </>
   );
 }
