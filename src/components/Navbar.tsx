abb3797e1821cfb10630c64a3e69951a120a0bbc
ccw2110 <carloscassinelli90@gmail.com>
2024-08-08 21:51:33 -0500

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+"use client"
+import React,{useState} from 'react'
+import Link from "next/link"
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'  
+import {motion} from 'framer-motion'
+
+
+const navLinks = [
+    {"title" : "About" , "path":"#About"},
+    {"title" : "Portfolio", "path": "#Portfolio"} 
+]
+
 
 const Navbar = () => {
+  const [nav,setNav] = useState(false)
+
+  const toggleNav = () =>{
+    setNav(!nav)
+  }
+
+   const closeNav = () =>{
+    setNav(false)
+  }
+
   return (
-    <div>Navbar</div>
+    <div className='text-white/70 pt-6' >
+      <div className='hidden md:flex items-center px-4 py-2 mx-auto max-w-[400px]'>
+        <ul>
+
+        </ul>
+      </div>
+    </div>
   )
 }
 
